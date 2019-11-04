3ddb99aff708edb27ea85b47d4fd5a4f913ef809
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2019-11-03 21:20:34 -0300

    feat: poc do fluxo padrão para o login do agente

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from wtforms import Form, RadioField, TextField, PasswordField, validators
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response, redirect
 from flask_wtf import FlaskForm
 from dotenv import load_dotenv
+import requests
 import os
 
 app = Flask(__name__)
@@ -20,7 +21,30 @@ class LoginForm(FlaskForm):
 def login_form():
     form = LoginForm(request.form)
     if form.validate_on_submit():
-        pass  # TODO
+        otrs_data = {
+            'User': form.username.data,
+            'Password': form.password.data,
+            'Action': 'Login',
+            'RequestedURL': None,
+            'Lang': 'en',
+            'TimeZoneOffset': 180  # TODO
+        }
+        # TODO: reinserir os parâmetros da url recebidos do otrs em caso de
+        # redirecionamento
+        with requests.post('http://localhost/otrs/index.pl',
+                           data=otrs_data, allow_redirects=False) as r:
+            if (r.status_code == 302
+                    and r.cookies.get('OTRSAgentInterface', None)):
+                response = redirect('http://localhost'+r.headers['location'])
+                # TODO: definir os outros parâmetros (host, expires etc.) de
+                # set_cookie respeitando os dados do cookie recebido.
+                response.set_cookie('OTRSAgentInterface',
+                                    r.cookies['OTRSAgentInterface'])
+                return response
+            # TODO: tratar outros casos: 200 (falha na autenticação), 500 etc
+            # TODO: Pegar a mensagem de erro do OTRS e inserir ela no nosso
+            # formulário (usar o beautifulsoup para pegar o texto do elemento
+            # .LoginBox>.ErrorBox>span)
     return render_template('form.html', form=form)
 
 
